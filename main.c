eaf1e2f376a8ada66356ab73c9b35cd408f8153a
cod-emminex <emminexy@yahoo.com>
2023-11-19 07:04:57 +0000

    worked on all files

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	unsigned int line_number = 0;
 
-	if (argc != 2)
+	if (argc != 0)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
