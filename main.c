3bba5b202db6b1eb91a6930bcd7817ed28a05197
cod-emminex <emminexy@yahoo.com>
2023-11-19 07:14:18 +0000

    worked on all files

--- main.c
+++ main.c
@@ -24,13 +24,13 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	unsigned int line_number = 0;
 
-	if (argc != 0)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	file = fopen(argv[0], "r");
+	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[0]);
